e86e316e34c8df3464d5b4235abb0c3a949a3eb0
Ricky Grullon <rgrullon8280@gmail.com>
2025-04-30 23:17:24 -0400

    add movement

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -19,7 +17,9 @@ def main():
         screen.fill('black')
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+
+        dt = clock.tick(FPS) / 1000
+        player.update(dt)
 
 if __name__ == '__main__':
     main()
--- player.py
+++ player.py
@@ -1,5 +1,9 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import (
+    PLAYER_RADIUS, 
+    PLAYER_TURN_SPEED,
+    PLAYER_SPEED,
+)
 import pygame
 
 class Player(CircleShape):
@@ -7,7 +11,6 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
-    # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -18,3 +21,30 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, 'white', self.triangle(), 2)
+
+    def rotate(self, dt):
+        degrees = PLAYER_TURN_SPEED * dt
+        self.rotation += degrees
+        print(self.rotation)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt * -1)
+
+        if keys[pygame.K_s]:
+            self.rotate(dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_r]:
+            self.move(dt * -1)
+
+    def move(self, dt):
+        movement_vector = pygame.Vector2(0, 1).rotate(self.rotation)
+        scalar = (PLAYER_SPEED * dt)
+        movement_vector *= scalar
+        self.position += movement_vector
+
